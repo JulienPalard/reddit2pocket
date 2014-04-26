8a96225e539b31c12084e68df9597c1513fda9db
Julien Palard <julien@palard.fr>
2014-04-26 15:49:52 +0200

    Pocket mark item as unread each time we push them, so don't.

--- reddit2pocket.py
+++ reddit2pocket.py
@@ -5,6 +5,25 @@ import re
 import sys
 import smtplib
 from email.mime.text import MIMEText
+import cPickle
+import os.path
+from contextlib import contextmanager
+from datetime import datetime
+
+
+@contextmanager
+def pickeled(pickle_file):
+    if os.path.isfile(pickle_file):
+        with open(pickle_file, 'rb') as f:
+            env = cPickle.load(f)
+    else:
+        env = {}
+    try:
+        yield env
+    finally:
+        pickle_file = os.path.expanduser("~/.reddit2pocket.pickle")
+        with open(pickle_file, 'wb') as f:
+            cPickle.dump(env, f, -1)
 
 
 def send_to_pocket(email_from, link):
@@ -18,9 +37,14 @@ def send_to_pocket(email_from, link):
     s.quit()
 
 
-for subreddit in sys.argv[2:]:
-    feed = feedparser.parse('http://www.reddit.com/r/%s/top/.rss?sort=top&t=week' % subreddit)
-    for i in range(5):
-        entry = feed['entries'][i]
-        source = re.findall(r'submitted by <a href.*?href=(["\'])(.*?)\1', entry['summary'])[0][1]
-        send_to_pocket(sys.argv[1], source)
+with pickeled(os.path.expanduser("~/.reddit2pocket.pickle")) as env:
+    if 'seen' not in env:
+        env['seen'] = {}
+    for subreddit in sys.argv[2:]:
+        feed = feedparser.parse('http://www.reddit.com/r/%s/top/.rss?sort=top&t=day' % subreddit)
+        for i in range(min(3, len(feed['entries']))):
+            entry = feed['entries'][i]
+            source = re.findall(r'submitted by <a href.*?href=(["\'])(.*?)\1', entry['summary'])[0][1]
+            if source not in env['seen']:
+                send_to_pocket(sys.argv[1], source)
+                env['seen'][source] = datetime.now()
